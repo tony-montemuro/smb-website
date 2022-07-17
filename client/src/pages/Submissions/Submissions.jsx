9e78797b9ac433b66fe0263629f98d1b048dbd15
Tony Montemuro <tonyamontemuro@gmail.com>
2022-07-17 16:58:27 -0500

    created functionality of recent submissions page

--- client/src/pages/Submissions/Submissions.jsx
+++ client/src/pages/Submissions/Submissions.jsx
@@ -1,8 +1,9 @@
 import { useEffect } from "react";
+import { Link } from "react-router-dom";
 import SubmissionInit from "./SubmissionsInit";
 
 function Submissions() {
-  const { gameList, submissions, checkForMod } = SubmissionInit();
+  const { loading, gameList, submissions, currentGame, setLoading, checkForMod, changeGame, removeSubmission } = SubmissionInit();
 
   useEffect(() => {
     checkForMod();
@@ -13,15 +14,87 @@ function Submissions() {
     // once submissions has a property for each game, this means that our querying has completed
     // and we can proceed
     if (gameList.length > 0 && Object.keys(submissions).length === gameList.length) {
-        console.log(submissions);
+        setLoading(false);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [submissions]);
 
   return (
-    <>
-    
-    </>
+    <div className="submissions">
+      <div className="submissions-header">
+        <h1>Recent Submissions</h1>
+        <p><i>Below is the list of recent submissions. Please go through and review each submission for each game.</i></p>
+        <p>If a submission is <b>valid</b>, just click the <b>Reviewed</b> button for that submission.</p>
+        <p>If you suspect a submission is <b>invalid</b>, first navigate to the board with the invalid
+        submission by clicking on the <b>Level Name</b>. If you still are suspicious, remove the record
+        by clicking the <b>Delete</b> button. Finally, navigate back to the <b>Recent Submissions</b> page, and
+        click the <b>Reviewed</b> button for that submission.</p>
+      </div>
+      {loading ?
+        <p>Loading...</p>
+        :
+        <div className="submissions-body">
+          <div className="selector">
+            <form>
+              <select
+                onChange={(e) => changeGame(e.target.value)}
+                value={currentGame}
+              >
+                {gameList.map(val => {
+                  return <option key={val.abb} value={val.abb}>{val.name}</option>
+                })}
+              </select>
+            </form>
+          </div>
+          <div className="submissions-list">
+            <table>
+              <thead>
+                <tr>
+                  <th>Submission Time</th>
+                  <th>Level Name</th>
+                  <th>User</th>
+                  <th>Record</th>
+                  <th>Proof</th>
+                  <th>Comment</th>
+                  <th>Reviewed</th>
+                </tr>
+              </thead>
+              <tbody>
+              {submissions[currentGame].map(val => {
+                return <tr key={val.created_at}>
+                  <td>{val.created_at}</td>
+                  <td>
+                    <Link to={`/games/${val.isMisc ? currentGame+"misc" : currentGame}/${val.isScore ? "score" : "time"}/${val.level_id}`}>
+                      {val.isMisc ? val.level_name+" (Misc)" : val.level_name}
+                    </Link></td>
+                  <td>
+                  {val.country ?
+                    <div className="country-icon">
+                      <img
+                          alt={val.country}
+                          src={`http://purecatamphetamine.github.io/country-flag-icons/3x2/${val.country}.svg`}
+                          style={{width: 30}}>
+                      </img>
+                    </div>
+                    :
+                    ""
+                  }
+                  <div><Link to={`/user/${val.user_id}`}>{val.username}</Link></div>
+                  </td>
+                  <td>{val.record}</td>
+                  <td>{val.proof !== "none" ? <a href={val.proof} target="_blank" rel="noopener noreferrer">☑️</a> : ''}</td>
+                  <td>{val.comment}</td>
+                  <td><button onClick={() => removeSubmission(val.created_at)}>☑️</button></td>
+                </tr>
+              })}
+              </tbody>
+            </table>
+            
+          </div>
+        </div>
+        
+      }
+    </div>
   );
 }
 
--- client/src/pages/Submissions/SubmissionsInit.jsx
+++ client/src/pages/Submissions/SubmissionsInit.jsx
@@ -4,8 +4,10 @@ import { supabase } from "../../components/SupabaseClient/SupabaseClient";
 
 const SubmissionInit = () => {
     // states
+    const [loading, setLoading] = useState(true);
     const [gameList, setGameList] = useState([]);
     const [submissions, setSubmissions] = useState({});
+    const [currentGame, setCurrentGame] = useState("smb1");
 
     // variables
     const navigate = useNavigate("/");
@@ -46,7 +48,9 @@ const SubmissionInit = () => {
         try {
             const { data: games, error, status } = await supabase
                 .from("games")
-                .select("abb, name, is_custom");
+                .select("abb, name, is_custom")
+                .order("is_custom")
+                .order("name");
 
             if (error && status !== 406) {
                 throw error;
@@ -59,21 +63,22 @@ const SubmissionInit = () => {
         }
     }
 
+    // function that actually performs the query based on the game param
     const querySubmissions = async (game) => {
         try {
             const { data, error, status } = await supabase
                 .from(`${game.abb}_recent_submissions`)
-                .select(
-                    "created_at",
-                    "profiles:user_id ( username, country )",
-                    "level_name",
-                    "level_id",
-                    "record",
-                    "isScore",
-                    "isMisc",
-                    "proof",
-                    "comment"
-                )
+                .select(`
+                    created_at,
+                    profiles:user_id ( username, country ),
+                    level_name,
+                    level_id,
+                    record,
+                    isScore,
+                    isMisc,
+                    proof,
+                    comment
+                `)
                 .order("created_at");
 
             if (error && status !== 406) {
@@ -92,7 +97,7 @@ const SubmissionInit = () => {
                 // since record is stored as a float, if the current submission is a score, we need to parse to
                 // integer type. otherwise, we need to fix the submission to two decimal places
                 const record = submission.record;
-                submission.record = submission.isScore ? parseInt(record) : record.toFixed(2);
+                submission.record = submission.isScore ? parseInt(record) : Number.parseFloat(record).toFixed(2);
                 delete submission.isScore;
             }
 
@@ -104,6 +109,7 @@ const SubmissionInit = () => {
         }
     }
 
+    // function that sets up queries for each game (to get recent submissions)
     const queryRecentSubmissions = async (games) => {
         for (const game of games) {
            querySubmissions(game);
@@ -115,7 +121,40 @@ const SubmissionInit = () => {
         navigate("/");
     }
 
-    return { gameList, submissions, checkForMod };
+    // function that will update the currentGame based on the user's selection
+    const changeGame = (game) => {
+        setCurrentGame(game);
+    }
+
+    // function that is called when a moderator has reviewed a submission
+    const removeSubmission = async (id) => {
+        // create a copy of the list of submissions without the reviewed submission
+        let submissionList = [];
+        for (let submission of submissions[currentGame]) {
+            if (submission.created_at !== id) {
+                submissionList.push(submission);
+            }
+        }
+        
+        // now, update the submissions object
+        setSubmissions(submissions => ({...submissions, [currentGame]: submissionList}));
+
+        // finally, make a query to the recent submissions page to remove this submission
+        try {
+            const { error } = await supabase
+                .from(`${currentGame}_recent_submissions`)
+                .delete()
+                .match({ created_at: id });
+
+            if (error) {
+                throw (error);
+            }
+        } catch (error) {
+            alert(error.message);
+        }
+    }
+
+    return { loading, gameList, submissions, currentGame, setLoading, checkForMod, changeGame, removeSubmission };
 }
 
 export default SubmissionInit;
