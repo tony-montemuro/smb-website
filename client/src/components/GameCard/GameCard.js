aeec0655a78025ef181cd3a0edfdf4b6b96218e5
Tony Montemuro <tonyamontemuro@gmail.com>
2023-03-17 18:42:30 -0500

    added function documentation to GameCard.js

--- client/src/components/GameCard/GameCard.js
+++ client/src/components/GameCard/GameCard.js
@@ -11,7 +11,15 @@ const GameCard = () => {
     // function for fetching box art
     const { retrieveImage } = Download();
     
-    // function that will return an image from an abbreviation (abb)
+    // FUNCTION 1: fetchBoxArt - retrieve a boxart image
+    // PRECONDITINOS (2 parameters):
+    // 1.) abb: a string value that represents a game
+    // 2.) imageReducer: an object with two fields:
+        // a.) reducer: the image reducer itself (state)
+        // b.) dispatchImages: the reducer function used to update the reducer
+    // POSTCONDITION:
+    // the async retrieveImages function is called, with the "boxart" parameter passed to specify that it's a box art.
+    // once the function returns the blob, the setImg() function must be called to update the img state
     const fetchBoxArt = async (abb, imageReducer) => {
         const blob = await retrieveImage(abb, imageReducer, "boxart");
         setImg(blob);
