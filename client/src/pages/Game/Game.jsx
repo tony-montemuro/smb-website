a70d641ed6e3c9163c02c3e591e9f5d55f4e5585
Tony Montemuro <tonyamontemuro@gmail.com>
2023-05-08 16:37:17 -0500

    began work on new game page

--- client/src/pages/Game/Game.jsx
+++ client/src/pages/Game/Game.jsx
@@ -1,7 +1,7 @@
 /* ===== IMPORTS ===== */
 import "./Game.css";
 import { GameContext } from "../../Contexts";
-import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { useContext, useState } from "react";
 import FrontendHelper from "../../helper/FrontendHelper";
 import ModeBody from "./ModeBody";
@@ -34,16 +34,30 @@ function Game() {
 
   /* ===== GAME COMPONENT ===== */
   return (
-    <>
-      { /* Game Header - this div renders general information related to the game */ }
-      <div className="game-header">
+    <div className="game">
+      <div className="game-info">
+        <div className="game-rules">
+          <h2>Rules</h2>
+          <ol>
+            <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo, quisquam veniam ipsam nulla ullam sint quam explicabo dolores labore, tempora perspiciatis quos delectus facere sapiente?</li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, ullam dolore eum voluptates maiores ad!</li>
+            <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quisquam vel alias ratione beatae molestias recusandae omnis, cum voluptates numquam delectus, voluptatum odit deserunt aliquid enim? Accusantium facere nihil deleniti iure!</li>
+          </ol>
+        </div>
+        <div className="game-recent-submissions">
+          <h2>Recent Submissions</h2>
+        </div>
+      </div>
+
+      { /* Game Level List - Specifies the category of levels, and renders a list of levels to select. */ }
+      <div className="game-level-list">
 
         { /* Two radio buttons to toggle between two category modes: main and misc. */ }
-        <div className="game-radio-buttons">
+        <div className="game-radio-btns">
           {[{ name: "main", alias: "Main" }, { name: "misc", alias: "Miscellaneous" }].map(category => {
             return (
-              <div key={ category.name } className={ `game-${ category.name }-btn` }>
-                <label htmlFor={ category.name }>{ category.alias } Charts:</label>
+              <div key={ category.name } className={ `game-radio-btn` }>
+                <label htmlFor={ category.name }>{ category.alias } Levels:</label>
                 <input 
                   type="radio" 
                   value={ category.name }
@@ -55,51 +69,19 @@ function Game() {
           })}
         </div>
 
+        { /* Level title */ }
+        <h2>{ capitalize(selectedRadioBtn) } Levels</h2>
+
+        { /* Level select table - Render a table of modes, each of which expands to a selection of levels */ }
+        <table>
+          { game.mode.filter(mode => selectedRadioBtn === "misc" ? mode.misc : !mode.misc).map(mode => {
+            return <ModeBody category={ selectedRadioBtn } modeName={ mode.name } key={ `${ selectedRadioBtn }_${ mode.name }` } />;
+          })}
+        </table>
+
       </div>
 
-      { /* Game Body - This div includes the level list, as well as links to game boards. */ }
-      <div className="game-body">
-
-        { /* Game Level List - Specifies the category of levels, and renders a list of levels to select. */ }
-        <div className="game-level-list">
-          <h2>{ capitalize(selectedRadioBtn) } Levels</h2>
-          <table>
-            { game.mode.filter(mode => selectedRadioBtn === "misc" ? mode.misc : !mode.misc).map(mode => {
-              return <ModeBody category={ selectedRadioBtn } modeName={ mode.name } key={ `${ selectedRadioBtn }_${ mode.name }` } />;
-            })}
-          </table>
-        </div>
-
-        {/* Game Boards */}
-        <div className="game-boards">
-
-          { /* This map function will render both a link to the time & score world record boards. */ }
-          { ["score" ,"time"].map(type => {
-            return (
-              <div key={ type } className={ `game-${ type }-wrs` }>
-                <h2>{ capitalize(type) } World Records</h2>
-                <Link to={ `/games/${ game.abb }/${ selectedRadioBtn }/${ type }` }>
-                  <p>{ selectedRadioBtn === "misc" ? capitalize(selectedRadioBtn) : null } { capitalize(type) } World Records</p>
-                </Link>
-              </div>
-            );
-          })}
-
-          { /* This map function will render both a link to the medal table and totalizer boards. */ }
-          { [{ name: "medals", alias: "Medal Tables" }, { name: "totalizer", alias: "Totalizers" }].map(boardType => {
-            return (
-              <div key={ boardType.name } className={ `game-${ boardType.name }` }>
-                <h2> { boardType.alias } </h2>
-                <Link to={ `/games/${ game.abb }/${ selectedRadioBtn }/${ boardType.name }` }>
-                  <p>{ selectedRadioBtn === "misc" ? capitalize(selectedRadioBtn) : null } Score & Time { boardType.alias }</p>
-                </Link>
-              </div>
-            );
-          })}
-
-        </div>
-      </div>
-    </>
+    </div>
   );
 };
 
