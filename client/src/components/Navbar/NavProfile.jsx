7fb7eb081db0001543aaed966bdd0a8b071852ea
Tony Montemuro <tonyamontemuro@gmail.com>
2023-05-31 13:36:01 -0500

    added comments to nav profile

--- client/src/components/Navbar/NavProfile.jsx
+++ client/src/components/Navbar/NavProfile.jsx
@@ -25,33 +25,49 @@ function NavProfile({ imageReducer }) {
   /* ===== NAV PROFILE COMPONENT ===== */
   return ( 
     <div className="nav-profile">
+
+      { /* Nav profile avatar: simply render the current user's avatar, which should link to the page to view their profile */ }
       <div className="nav-profile-avatar">
         <Link to={ `/user/${ user.profile.id }` } title="View Profile" >
           <Avatar profileId={ user.profile.id } size={ 60 } imageReducer={ imageReducer } />
         </Link>
       </div>
+
+      { /* Nav profile details: includes the user's username, and profile links  */ }
       <div className="nav-profile-details">
+
+        { /* Username: render the user's username */ }
         <div className="nav-profile-username">
           <Username country={ user.profile.country.iso2 } profileId={ user.profile.id } username={ user.profile.username } />
         </div>
+
+        { /* Links: all the user's different links */ }
         <div className="nav-profile-links">
+
+          { /* Profile settings link - profile icon that links to the profile page */ }
           <div className="nav-profile-link">
             <Link to="/profile">
               <ManageAccountsIcon titleAccess="Profile Settings" />
             </Link>
           </div>
+
+          { /* Notifications link - bell icon that links to the notifications page */ }
           <div id="nav-profile-notifications" className="nav-profile-link">
             { user.notifications.length > 0 ?
+              // If the user has one or more notifications, render the number of notifications next to a full bell
               <Link to="/notifications">
                 <span>{ user.notifications.length }</span>
                 <NotificationsIcon titleAccess="Notifications" />
               </Link>
             :
+              // Otherwise, render an empty bell
               <Link to="/notifications">
                 <NotificationsNoneOutlinedIcon titleAccess="Notifications" />
               </Link>
             }
           </div>
+
+          { /* Recent submissions link - list icon that links to the recent submissions page (for moderators only) */ }
           { user.is_mod &&
             <div className="nav-profile-link">
               <Link to="/submissions">
@@ -59,6 +75,8 @@ function NavProfile({ imageReducer }) {
               </Link>
             </div>
           }
+
+          { /* Logout button - button that allows the user to log out of their account */ }
           <div className="nav-profile-link">
             <button id="nav-profile-signout-btn" onClick={ signOut }>
               <LogoutIcon titleAccess="Sign Out" />
