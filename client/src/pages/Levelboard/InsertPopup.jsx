30beda0d176d9bdc5fd5200e7ccc4d4a65d85bf5
Tony Montemuro <tonyamontemuro@gmail.com>
2023-06-20 14:09:57 -0500

    cleaned up submission (insert) form for levelboard page

--- client/src/pages/Levelboard/InsertPopup.jsx
+++ client/src/pages/Levelboard/InsertPopup.jsx
@@ -25,6 +25,8 @@ function InsertPopup({ popup, setPopup, submissions }) {
   const profiles = staticCache.profiles;
   const allSubmissions = submissions.all;
   const liveSubmissions = submissions.live;
+  const TEXT_AREA_ROWS = 5;
+  const TEXT_AREA_COLS = 20;
 
   /* ===== STATES & FUNCTIONS ===== */
 
@@ -55,139 +57,148 @@ function InsertPopup({ popup, setPopup, submissions }) {
         </div>
 
         { /* Levelboard submit - contains the form header and form for submitting submissions to the database */ }
-        <div className="levelboard-submit">
+        <div className="levelboard-submit-wrapper">
+          <div className="levelboard-submit">
 
-          { /* Form header - specifies the type of submission */ }
-          <h2>Submit a { capitalize(type) }:</h2>
+            { /* Form header - specifies the type of submission */ }
+            <h2>Submit a { capitalize(type) }:</h2>
 
-          { /* Submission form - allows users to submit a record to the database */ }
-          <form onSubmit={ (e) => handleSubmit(e, allSubmissions, liveSubmissions) }>
+            { /* Submission form - allows users to submit a record to the database */ }
+            <form onSubmit={ (e) => handleSubmit(e, allSubmissions, liveSubmissions) }>
 
-            { /* User input: allows a moderator to select which user they want to submit on behalf of from a dropdown menu. 
-            NOTE: this form input is reserved for moderators only! */ }
-            { user.is_mod &&
+              { /* User input: allows a moderator to select which user they want to submit on behalf of from a dropdown menu. 
+              NOTE: this form input is reserved for moderators only! */ }
+              { user.is_mod &&
+                <div className="levelboard-input-group">
+                  <label htmlFor="profile_id">User: </label>
+                  <select id="profile_id" value={ form.values.profile_id } onChange={ (e) => handleChange(e) }>
+                    { profiles.map((profile) => (
+                      <option key={ profile.id } value={ profile.id }>{ profile.username }</option>
+                    ))}
+                  </select>
+                </div>
+              }
+
+              { /* Record input: allows the user to input their record for the submission */ }
               <div className="levelboard-input-group">
-                <label htmlFor="profile_id">User: </label>
-                <select id="profile_id" value={ form.values.profile_id } onChange={ (e) => handleChange(e) }>
-                  { profiles.map((profile) => (
-                    <option key={ profile.id } value={ profile.id }>{ profile.username }</option>
-                  ))}
-                </select>
-              </div>
-            }
-
-            { /* Record input: allows the user to input their record for the submission */ }
-            <div className="levelboard-input-group">
-              <label htmlFor="record">{ capitalize(type) }: </label>
-              <input 
-                id="record"
-                type="number"
-                value={ form.values.record }
-                onChange={ (e) => handleChange(e) }
-              />
-
-              { /* If the error.record field is defined, render that underneath the record field. */ }
-              { form.error.record && <p>{ form.error.record }</p> }
-
-            </div>
-
-            { /* Submission date input: allows the user to input the date they achieved their submission. */ }
-            <div className="levelboard-input-group">
-              <label htmlFor="submitted_at">Date: </label>
-              <input 
-                id="submitted_at" 
-                type="date" 
-                min={ game.release_date } 
-                max={ dateB2F() }
-                value={ form.values.submitted_at }
-                onChange={ (e) => handleChange(e) }
-              />
-            </div>
-
-            { /* Submission region input: allows the user to select the region they achieved their submission on from a dropdown. */ }
-            <div className="levelboard-input-group">
-              <label htmlFor="region_id">Region: </label>
-              <select id="region_id" value={ form.values.region_id } onChange={ (e) => handleChange(e) }>
-                { game.region.map(region => (
-                  <option key={ region.id } value={ region.id }>{ region.region_name }</option>
-                ))}
-              </select>
-            </div>
-
-            { /* Submission monkey input: allows the user to select the monkey they achieved their submission on from a dropdown. */ }
-            <div className="levelboard-input-group">
-              <label htmlFor="monkey_id">Monkey: </label>
-              <select id="monkey_id" value={ form.values.monkey_id } onChange={ (e) => handleChange(e) }>
-                { game.monkey.map((monkey) => (
-                  <option key={ monkey.id } value={ monkey.id }>{ monkey.monkey_name }</option>
-                ))}
-              </select>
-            </div>
-
-            { /* Submission proof input: allows the user to input the proof of submission. */ }
-            <div className="levelboard-input-group">
-              <label htmlFor="proof">Proof: </label>
-              <input 
-                id="proof"
-                type="url"
-                value={ form.values.proof }
-                onChange={ (e) => handleChange(e) }
-              />
-
-              { /* If the error.proof field is defined, render that underneath the proof field. */ }
-              { form.error.proof && <p>{ form.error.proof }</p> }
-
-            </div>
-
-            { /* Submission comment input: allows the user to input a comment regarding their submission. */ }
-            <div className="levelboard-input-group">
-              <label htmlFor="comment">Comment (optional): </label>
-              <input 
-                id="comment"
-                type="text"
-                value={ form.values.comment }
-                onChange={ (e) => handleChange(e) }
-                disabled={ user.profile.id !== form.values.profile_id }
-              />
-
-              { /* If the error.comment field is defined, render that underneath the proof field. */ }
-              { form.error.proof && <p>{ form.error.comment }</p> }
-
-            </div>
-
-            { /* Submission message input: allows a moderator to include a message for the submission, since users will recieve a
-            notification when a moderator submits or updates a submission on behalf of them. The message is included in this.
-            NOTE: This input form should only render if the current user is a moderator, and the form user is different than the mod. */ }
-            { user.is_mod && user.profile.id !== form.values.profile_id &&
-              <div className="levelboard-input-group">
-                <label htmlFor="message">Leave a message (optional): </label>
+                <label htmlFor="record">{ capitalize(type) }: </label>
                 <input 
-                  id="message"
-                  type="text"
-                  value={ form.values.message }
+                  id="record"
+                  className="levelboard-submit-record"
+                  type="number"
+                  value={ form.values.record }
                   onChange={ (e) => handleChange(e) }
                 />
 
-                { /* If the error.message field is defined, render that underneath the proof field. */ }
-                { form.error.message  && <p>{ form.error.message }</p> }
+                { /* If the error.record field is defined, render that underneath the record field. */ }
+                { form.error.record && <p>{ form.error.record }</p> }
+
               </div>
-            }
 
-            { /* Submission live checkbox: allows the user to specify whether or not the submission was live or not. */ }
-            <div className="levelboard-input-group">
-              <label htmlFor="live">Live Run: </label>
-              <input
-                id="live"
-                type="checkbox"
-                checked={ form.values.live }
-                onChange={ (e) => handleChange(e) }
-              />
-            </div>
+              { /* Submission date input: allows the user to input the date they achieved their submission. */ }
+              <div className="levelboard-input-group">
+                <label htmlFor="submitted_at">Date: </label>
+                <input 
+                  id="submitted_at" 
+                  type="date" 
+                  min={ game.release_date } 
+                  max={ dateB2F() }
+                  value={ form.values.submitted_at }
+                  onChange={ (e) => handleChange(e) }
+                />
+              </div>
 
-            { /* Form submission button: submits the form. NOTE: button is disabled if the submitting field of form is true. */ }
-            <button disabled={ form.submitting }>Submit</button>
+              { /* Submission region input: allows the user to select the region they achieved their submission on from a dropdown. */ }
+              <div className="levelboard-input-group">
+                <label htmlFor="region_id">Region: </label>
+                <select id="region_id" value={ form.values.region_id } onChange={ (e) => handleChange(e) }>
+                  { game.region.map(region => (
+                    <option key={ region.id } value={ region.id }>{ region.region_name }</option>
+                  ))}
+                </select>
+              </div>
 
-          </form>
+              { /* Submission monkey input: allows the user to select the monkey they achieved their submission on from a dropdown. */ }
+              <div className="levelboard-input-group">
+                <label htmlFor="monkey_id">Monkey: </label>
+                <select id="monkey_id" value={ form.values.monkey_id } onChange={ (e) => handleChange(e) }>
+                  { game.monkey.map((monkey) => (
+                    <option key={ monkey.id } value={ monkey.id }>{ monkey.monkey_name }</option>
+                  ))}
+                </select>
+              </div>
+
+              { /* Submission proof input: allows the user to input the proof of submission. */ }
+              <div className="levelboard-input-group">
+                <label htmlFor="proof">Proof: </label>
+                <input 
+                  id="proof"
+                  type="url"
+                  value={ form.values.proof }
+                  onChange={ (e) => handleChange(e) }
+                />
+
+                { /* If the error.proof field is defined, render that underneath the proof field. */ }
+                { form.error.proof && <p>{ form.error.proof }</p> }
+
+              </div>
+
+              { /* Submission live checkbox: allows the user to specify whether or not the submission was live or not. */ }
+              <div className="levelboard-input-group">
+                <label htmlFor="live">Live Proof: </label>
+                <input
+                  id="live"
+                  type="checkbox"
+                  checked={ form.values.live }
+                  onChange={ (e) => handleChange(e) }
+                />
+              </div>
+
+              { /* Submission comment input: allows the user to input a comment regarding their submission. */ }
+              <div className="levelboard-textarea-group">
+                <label htmlFor="comment">Comment (optional): </label>
+                <textarea 
+                  id="comment"
+                  type="text"
+                  value={ form.values.comment }
+                  onChange={ (e) => handleChange(e) }
+                  disabled={ user.profile.id !== form.values.profile_id }
+                  rows={ TEXT_AREA_ROWS }
+                  cols={ TEXT_AREA_COLS }
+                />
+
+                { /* If the error.comment field is defined, render that underneath the proof field. */ }
+                { form.error.proof && <p>{ form.error.comment }</p> }
+
+              </div>
+
+              { /* Submission message input: allows a moderator to include a message for the submission, since users will recieve a
+              notification when a moderator submits or updates a submission on behalf of them. The message is included in this.
+              NOTE: This input form should only render if the current user is a moderator, and the form user is different than the mod. */ }
+              { user.is_mod && user.profile.id !== form.values.profile_id &&
+                <div className="levelboard-textarea-group">
+                  <label htmlFor="message">Leave a message (optional): </label>
+                  <textarea 
+                    id="message"
+                    type="text"
+                    value={ form.values.message }
+                    onChange={ (e) => handleChange(e) }
+                    rows={ TEXT_AREA_ROWS }
+                    cols={ TEXT_AREA_COLS }
+                  />
+
+                  { /* If the error.message field is defined, render that underneath the proof field. */ }
+                  { form.error.message  && <p>{ form.error.message }</p> }
+                </div>
+              }
+
+              { /* Form submission button: submits the form. NOTE: button is disabled if the submitting field of form is true. */ }
+              <div className="levelboard-submit-btn-wrapper">
+                <button disabled={ form.submitting }>Submit</button>
+              </div>
+
+            </form>
+          </div>
         </div>
       </div>
     </div>
