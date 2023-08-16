6a92fa3bdcf1493799a6c88385fdb4edf8eda599
Tony Montemuro <tonyamontemuro@gmail.com>
2023-08-16 11:54:00 -0500

    fixed minor bug regarding rankings panel rendering

--- client/src/components/GameLayout/GameLayout.jsx
+++ client/src/components/GameLayout/GameLayout.jsx
@@ -88,7 +88,7 @@ function GameLayout({ imageReducer }) {
         { /* Game Layout Sidebar - a set of links used to navigate various game pages. */ }
         <div className="game-layout-body-sidebar">
           { getGameCategories(game).map(category => {
-            return <GameLayoutInfo abb={ game.abb } category={ "main" } key={ category } />
+            return <GameLayoutInfo abb={ game.abb } category={ category } key={ category } />
           })}
         </div>
         
