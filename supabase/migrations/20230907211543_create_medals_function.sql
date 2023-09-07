091eb869b085edd321b5dd2bf221901889e5b555
Tony Montemuro <tonyamontemuro@gmail.com>
2023-09-07 16:17:08 -0500

    added the get_medals function

---
+++ supabase/migrations/20230907211543_create_medals_function.sql
@@ -0,0 +1,37 @@
+CREATE OR REPLACE FUNCTION get_medals(abb text, category category_t, score boolean)
+RETURNS json
+LANGUAGE sql
+AS $$
+WITH ranked_submissions AS (
+  SELECT
+    level_id,
+    id,
+    username,
+    country,
+    "position"
+  FROM get_ranked_submissions(abb, category, score, true)
+),
+medal_counts AS (
+  SELECT
+    id,
+    username,
+    country,
+    COUNT(*) FILTER (WHERE "position" = 1 AND (SELECT COUNT(*) FROM ranked_submissions rs WHERE rs.level_id = ranked_submissions.level_id AND rs."position" = 1) = 1) AS platinum,
+    COUNT(*) FILTER (WHERE "position" = 1 AND (SELECT COUNT(*) FROM ranked_submissions rs WHERE rs.level_id = ranked_submissions.level_id AND rs."position" = 1) > 1) AS gold,
+    COUNT(*) FILTER (WHERE "position" = 2) AS silver,
+    COUNT(*) FILTER (WHERE "position" = 3) AS bronze
+  FROM ranked_submissions
+  GROUP BY id, username, country
+)
+SELECT CASE WHEN category IN ('main', 'misc') THEN COALESCE((json_agg(row_to_json(medals_row))), '[]'::json) ELSE '[]'::json END
+FROM (
+  SELECT
+    (SELECT jsonb_build_object('country', mc.country, 'id', mc.id, 'username', mc.username)) AS profile,
+    platinum,
+    gold,
+    silver,
+    bronze
+  FROM medal_counts mc
+  ORDER BY platinum DESC, gold DESC, silver DESC, bronze DESC
+) medals_row
+$$;
