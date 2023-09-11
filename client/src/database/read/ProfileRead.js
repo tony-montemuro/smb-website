96a2c04cbed1571afe79c1d670e917b92fd89378
Tony Montemuro <tonyamontemuro@gmail.com>
2023-09-11 17:24:03 -0500

    created function for searching for profiles

--- client/src/database/read/ProfileRead.js
+++ client/src/database/read/ProfileRead.js
@@ -78,7 +78,40 @@ const ProfileRead = () => {
         }
     };
 
-    return { queryProfiles, queryUserProfile };
+    // FUNCTION 3: searchForProfiles - function that grabs a subset of profiles, according to the users input
+    // PRECONDITIONS (3 parameters):
+    // 1.) userInput: a string, which a user has entered in an attempt to find a user profile. we use this value to attempt
+    // to match 0 or more profiles to this value
+    // 2.) start: an integer, representing the first profile to be selected
+    // 3.) end: an integer, representing last profile to be selected
+    // POSTCONDITIONS (2 returns, 2 possible outcomes):
+    // if the query is successful, an object with two fields is returned:
+        // 1.) profiles: an array of profile objects, which have a substring matching the user input (case-insensitive)
+        // 2.) count: the total number of profiles that match the user input. in some cases, this number will be larger 
+        // than `profiles.length`
+    // if the query fails, this function throws an error, which should be handled by the caller function
+    const searchForProfiles = async(userInput, start, end) => {
+        try {
+            const { data: profiles, count, error } = await supabase
+                .from("profile")
+                .select("country, discord, id, username, twitch_username, twitter_handle, youtube_handle", { count: "exact" })
+                .ilike("username", `%${ userInput }%`)
+                .order("username")
+                .range(start, end);
+
+            // error handling
+            if (error) {
+                throw error;
+            }
+
+            return { profiles, count };
+
+        } catch (error) {
+            throw error;
+        };
+    };
+
+    return { queryProfiles, queryUserProfile, searchForProfiles };
 };
 
 /* ===== EXPORTS ===== */
