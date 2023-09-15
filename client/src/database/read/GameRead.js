eeb78dd8a317aebb3f983b8ed5500b11fe3dbf5c
Tony Montemuro <tonyamontemuro@gmail.com>
2023-09-15 11:56:47 -0500

    removed dependency of 'games' cache on GameSelect page

--- client/src/database/read/GameRead.js
+++ client/src/database/read/GameRead.js
@@ -145,6 +145,51 @@ const GameRead = () => {
         }
     };
 
+    // FUNCTION 3: searchForGames - function that grabs a subset of games, according to the users input
+    // PRECONDITIONS (4 parameters):
+    // 1.) userInput: a string, which a user has entered in an attempt to find a user profile. we use this value to attempt
+    // to match 0 or more games to this value
+    // 2.) start: an integer, representing the first game to be selected
+    // 3.) end: an integer, representing last game to be selected
+    // 4.) gameTypeFilter: a value which determines how the games should be filtered. can be 3 values: "main", "custom", or undefined
+    // POSTCONDITIONS (2 returns, 2 possible outcomes):
+    // if the query is successful, an object with two fields is returned:
+        // 1.) games: an array of gane objects, which have a substring matching the user input (case-insensitive)
+        // 2.) count: the total number of games that match the user input. in some cases, this number will be larger 
+        // than `games.length`
+    // if the query fails, this function throws an error, which should be handled by the caller function
+    const searchForGames = async(userInput, start, end, gameTypeFilter) => {
+        // first, let's generate our `custom` field filter
+        let customFilter = [];
+        if (!gameTypeFilter || gameTypeFilter === "custom") {
+            customFilter.push(true);
+        }
+        if (!gameTypeFilter || gameTypeFilter === "main") {
+            customFilter.push(false)
+        } 
+
+        try {
+            const { data: games, count, error } = await supabase
+                .from("game")
+                .select("abb, custom, name", { count: "exact" })
+                .in("custom", customFilter)
+                .ilike("name", `%${ userInput }%`)
+                .order("custom")
+                .order("id")
+                .range(start, end);
+
+            // error handling
+            if (error) {
+                throw error;
+            }
+
+            return { games, count };
+
+        } catch (error) {
+            throw error;
+        };
+    };
+
     // FUNCTION 1: queryGames - async function that makes a call to supabase to get an array of all the games
     // PRECONDITIONS: NONE
     // POSTCONDITIONS (2 possible outcomes):
@@ -222,7 +267,7 @@ const GameRead = () => {
         }
     };
 
-    return { queryGame, queryGamesInfo, queryGames };
+    return { queryGame, queryGamesInfo, searchForGames, queryGames };
 };
 
 /* ===== EXPORTS ===== */
--- client/src/pages/GameSelect/GameSelect.jsx
+++ client/src/pages/GameSelect/GameSelect.jsx
@@ -1,54 +1,114 @@
 /* ===== IMPORTS ===== */
 import "./GameSelect.css";
 import { Link } from "react-router-dom";
-import { StaticCacheContext } from "../../utils/Contexts";
-import { useContext } from "react";
+import { useEffect, useState } from "react";
 import BoxArt from "../../components/BoxArt/BoxArt.jsx";
-import GameSearchBar from "./GameSearchBar.jsx";
+import GameSelectLogic from "./GameSelect.js";
+import SearchBarInput from "../../components/SearchBarInput/SearchBarInput";
+import PageControls from "../../components/PageControls/PageControls.jsx";
 
 function GameSelect({ imageReducer }) {
+  /* ===== STATES & FUNCTIONS ===== */
+  const [pageNum, setPageNum] = useState(1);
+  const [searchInput, setSearchInput] = useState("");
+  const [gameTypeFilter, setGameTypeFilter] = useState(undefined);
+
+  // states & functions from the js file
+  const { games, updateResults } = GameSelectLogic();
+
   /* ===== VARIABLES ===== */
   const BOX_WIDTH = 200;
+  const GAMES_PER_PAGE = 20;
+  const filters = [
+    { name: "Both", value: undefined }, 
+    { name: "Main Games", value: "main" }, 
+    { name: "Custom Games", value: "custom" }
+  ];
+  let gameTypes = [];
+  games.data.forEach(game => {
+    const gameType = game.custom ? "Custom" : "Main";
+    if (!(gameTypes.includes(gameType))) {
+      gameTypes.push(gameType);
+    }
+  });
 
-  /* ===== CONTEXTS ===== */
+  /* ===== EFFECTS ===== */
 
-	// static cache state from static cache context
-  const { staticCache } = useContext(StaticCacheContext);
+  // code that is executed when the component mounts OR when user makes changes pages AND/OR makes a change to the search bar
+  useEffect(() => {
+    updateResults(searchInput, GAMES_PER_PAGE, pageNum, gameTypeFilter);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [pageNum]);
+
+  // code that is executed when the component mounts OR when users makes a change to searchInput OR gameTypeFilter
+  useEffect(() => {
+    if (pageNum === 1) {
+      updateResults(searchInput, GAMES_PER_PAGE, pageNum, gameTypeFilter);
+    } else {
+      setPageNum(1);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [searchInput, gameTypeFilter]);
 
   /* ===== GAME SELECT COMPONENT ===== */
   return (
     <div className="game-select">
 
-      { /* Page Header - render the name of the page, and the game search bar component */ }
+      { /* Page Header - render the name of the page, and game select filters */ }
       <div className="game-select-header">
         <h1>Games</h1>
-        <GameSearchBar imageReducer={ imageReducer } />
-      </div>
+        <div className="game-select-filters">
 
-      { /* Render two separate game select menus: one for main games, and one for custom games. */ }
-      { ["Main", "Custom"].map(type => {
+          { /* Render a search bar input for games */ }
+          <SearchBarInput itemType={ "Games" } input={ searchInput } setInput={ setSearchInput } width={ "75%" } />
+
+          { /* Render buttons to allow user to filter games by their type */ }
+          <div className="game-select-filter-btns">
+            { filters.map(filter => {
+              return (
+                <button 
+                  className={ `game-select-filter-btn${ gameTypeFilter === filter.value ? " game-select-filter-btn-selected" : "" }` }
+                  onClick={ () => setGameTypeFilter(filter.value) }
+                  key={ filter.name }
+                >
+                  { filter.name }
+                </button>
+              );
+            })}
+          </div>
+
+        </div>
+      </div>    
+
+      { /* Render a game select menu for each game type. */ }
+      { gameTypes.map(type => {
         return (
           <div key={ type } className="game-select-body">
             <h2>{ type } Games</h2>
             <div className="game-select-cards">
 
-              { /* If the games data has loaded from the database, we will filter and map each one to the screen as a card.
-              Otherwise, render a loading component. */ }
-              { staticCache.games.length > 0 ?
-                staticCache.games.filter(game => type === "Custom" ? game.custom : !game.custom).map(game => (
-                  <div className="game-select-card" key={ game.abb }>
-                    <Link to={ { pathname: `/games/${ game.abb }` } }>
-                      <BoxArt game={ game } imageReducer={ imageReducer } width={ BOX_WIDTH } />
-                      <p>{ game.name } </p>
-                    </Link>
-                  </div>
-                ))
-              :
-                <p>Loading...</p>
-              }
+              { /* Filter and map the game types to the screen as a card. */ }
+              { games.data.filter(game => type === "Custom" ? game.custom : !game.custom).map(game => (
+                <div className="game-select-card" key={ game.abb }>
+                  <Link to={ { pathname: `/games/${ game.abb }` } }>
+                    <BoxArt game={ game } imageReducer={ imageReducer } width={ BOX_WIDTH } />
+                    <p>{ game.name } </p>
+                  </Link>
+                </div>
+              ))}
+
             </div>
           </div>
       )})}
+
+      { /* Render page controls at the bottom of this container */ }
+      <PageControls 
+        totalItems={ games.total }
+        itemsPerPage={ GAMES_PER_PAGE }
+        pageNum={ pageNum }
+        setPageNum={ setPageNum }
+        itemName={ "Games" }
+      />
     </div>
   );
 };
