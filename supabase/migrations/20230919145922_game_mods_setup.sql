b2898c426adc5f402912f1ae29a09b1748ec233b
Tony Montemuro <tonyamontemuro@gmail.com>
2023-09-19 10:17:15 -0500

    created new table for game-specific moderators

---
+++ supabase/migrations/20230919145922_game_mods_setup.sql
@@ -0,0 +1,14 @@
+CREATE TABLE game_profile (
+    game text not null,
+    profile integer not null,
+    constraint game_profile_game_fkey foreign key (game) references game (abb),
+    constraint game_profile_profile_fkey foreign key (profile) references profile (id),
+    primary key (game, profile)
+);
+
+ALTER TABLE game_profile ENABLE ROW LEVEL SECURITY;
+
+CREATE POLICY "Enable read access for all users" ON game_profile
+FOR SELECT
+TO authenticated
+USING (true);
