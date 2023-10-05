7b702674e0dccf8f2cc6414447a256d9ac8dc7a9
Tony Montemuro <tonyamontemuro@gmail.com>
2023-10-04 23:33:34 -0500

    added styling to the login page

--- client/src/pages/SignIn/SignIn.jsx
+++ client/src/pages/SignIn/SignIn.jsx
@@ -1,15 +1,43 @@
 /* ===== IMPORTS ===== */
-import "./SignIn.css";
 import { Auth } from "@supabase/auth-ui-react";
 import { supabase } from "../../database/SupabaseClient";
 import { ThemeSupa } from "@supabase/auth-ui-shared";
 import { useContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { UserContext } from "../../utils/Contexts";
+import styles from "./SignIn.module.css";
+import LoginImage from "../../assets/png/login.png";
 
 function SignIn() {
   /* ===== VARIABLES ===== */
   const navigate = useNavigate();
+  const theme = { 
+    ...ThemeSupa, 
+    dark: {
+      ...ThemeSupa.dark,
+      colors: {
+        ...ThemeSupa.dark.colors,
+        brand: "rgb(var(--color-nav))",
+        brandAccent: "rgb(var(--color-nav-hover))",
+        brandButtonText: "rgb(var(--color-text))",
+      }
+    },
+    default: {
+      ...ThemeSupa.default,
+      fonts: {
+        bodyFontFamily: "var(--font)",
+        buttonFontFamily: "var(--font)",
+        inputFontFamily: "var(--font)",
+        labelFontFamily: "var(--font)"
+      },
+      fontSizes: {
+        baseBodySize: "15px",
+        baseInputSize: "16px",
+        baseLabelSize: "16px",
+        baseButtonSize: "16px"
+      }
+    }
+  };
 
   /* ===== CONTEXTS ===== */
 
@@ -18,7 +46,7 @@ function SignIn() {
 
   /* ===== EFFECTS ===== */
 
-  // code that is executed each time the user context state is updated
+  // code that is executed each time the user state is updated
   useEffect(() => {
     if (user.id) {
       navigate("/profile");
@@ -28,31 +56,22 @@ function SignIn() {
 
   /* ===== SIGN IN COMPONENT ===== */
   return (
-    <div className="signin">
+    <div className={ styles.signIn }>
 
-      { /* Sign in left: render the sign in form (left 35% of screen) */ }
-      <div className="signin-left">
+      { /* Left: render the sign in form */ }
+      <div className={ styles.left }>
         <h2>SMB Elite</h2>
         <Auth 
           supabaseClient={ supabase }
-          appearance={ { 
-            theme: ThemeSupa,
-            variables: {
-              default: {
-                colors: {
-                  brand: 'blue',
-                  brandAccent: 'darkblue',
-                },
-              },
-            }
-          } }
-          providers={[]}
+          theme="dark"
+          appearance={ { theme } }
+          providers={ [] }
         />
       </div>
 
-      { /* Sign in right: render a fancy image (right 65% of screen) */ }
-      <div className="signin-right">
-        <h1>&lt;insert image here&gt;</h1>
+      { /* Right: render a fancy image (1000px) */ }
+      <div className={ styles.right }>
+        <img src={ LoginImage } alt="Login decorative feature"></img>
       </div>
 
     </div>
