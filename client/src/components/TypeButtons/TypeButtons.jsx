9c0cafeef5a733538ab2e6c1d5840a0faa92c42e
Tony Montemuro <tonyamontemuro@gmail.com>
2023-10-09 00:10:43 -0500

    created new component for rendering level type buttons

---
+++ client/src/components/TypeButtons/TypeButtons.jsx
@@ -0,0 +1,32 @@
+/* ===== IMPORTS ===== */
+import { useNavigate } from "react-router-dom";
+import styles from "./TypeButtons.module.css";
+
+function TypeButtons({ abb, category, level }) {
+  /* ===== VARIABLES ===== */
+  const navigate = useNavigate();
+
+  /* ===== TYPE BUTTONS COMPONENT ===== */
+  return (
+    <div className={ styles.typeBtns }>
+
+      {/* Score button */}
+      { (level.chart_type === "both" || level.chart_type === "score") &&
+        <button type="button" onClick={ () => navigate(`/games/${ abb }/${ category }/score/${ level.name }`) }>
+          Score
+        </button>
+      }
+
+      {/* Time button */}
+      { (level.chart_type === "time" || level.chart_type === "both") &&
+        <button type="button" onClick={ () => navigate(`/games/${ abb }/${ category }/time/${ level.name }`) }>
+          Time
+        </button>
+      }
+
+    </div>
+  );
+};
+
+/* ===== EXPORTS ===== */
+export default TypeButtons;
