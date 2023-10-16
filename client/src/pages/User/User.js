52492e11d5bf61f7c8d54ab313a2a19450957205
Tony Montemuro <tonyamontemuro@gmail.com>
2023-10-15 22:49:21 -0500

    added styling to the user page

---
+++ client/src/pages/User/User.js
@@ -0,0 +1,63 @@
+/* ===== IMPORTS ===== */
+import { ProfileContext } from "../../utils/Contexts";
+import { useContext, useMemo } from "react";
+import { useNavigate } from "react-router-dom";
+
+const User = () => {
+    /* ===== VARIABLES ===== */
+    const navigate = useNavigate();
+
+    /* ===== CONTEXTS ===== */
+
+    // profile state from profile context
+    const { profile } = useContext(ProfileContext);
+
+    /* ===== MEMOS ===== */
+
+    // memo to store search params object to prevent unnecessary rerenders of recent submissions
+    const searchParams = useMemo(() => {
+        const params = new URLSearchParams();
+        params.append("profile_id", profile.id);
+        return params;
+    }, [profile.id]);
+
+    // memo to store the array of socials (avoid recompute)
+    const socials = useMemo(() => {
+        const socialKeys = ["youtube_handle", "twitch_username", "twitter_handle", "discord"];
+        const socials = [];
+        socialKeys.forEach(key => {
+            if (profile[key]) {
+                socials.push(profile[key]);
+            }
+        });
+        return socials;
+    }, [profile]);
+
+    // memo to store the array of details (avoid recompute)
+    const details = useMemo(() => {
+        const detailKeys = ["bio", "birthday"];
+        const details = [];
+        detailKeys.forEach(key => {
+            if (profile[key]) {
+                details.push(profile[key]);
+            }
+        });
+        return details; 
+    }, [profile]);
+
+    /* ===== FUNCTIONS ===== */
+
+    // FUNCTION 1: code that executes when user clicks a game row
+    // PRECONDITIONS (1 parameter):
+    // 1.) game: an object containing the `abb` key, which refers to the primary key of a game
+    // POSTCONDITIONS (1 possible outcome):
+    // the user is navigated to the page associated with the game
+    const onGameRowClick = game => {
+        navigate(`/games/${ game.abb }`);
+    };
+
+    return { searchParams, socials, details, onGameRowClick };
+};
+
+/* ===== EXPORTS ===== */
+export default User;
--- client/src/pages/User/User.jsx
+++ client/src/pages/User/User.jsx
@@ -1,13 +1,16 @@
 /* ===== IMPORTS ===== */
-import "./User.css";
-import { Link } from "react-router-dom";
 import { ProfileContext } from "../../utils/Contexts";
-import { useContext, useMemo } from "react";
-import BoxArt from "../../components/BoxArt/BoxArt.jsx";
+import { useContext } from "react";
+import styles from "./User.module.css";
+import Container from "../../components/Container/Container.jsx";
 import DiscordLogo from "../../components/DiscordLogo/DiscordLogo.jsx";
+import EmbedHelper from "../../helper/EmbedHelper";
 import EmbededVideo from "../../components/EmbededVideo/EmbededVideo.jsx";
+import GameRow from "../../components/GameRow/GameRow.jsx";
+import Items from "../../components/Items/Items.jsx";
 import RecentSubmissionsTable from "../../components/RecentSubmissionsTable/RecentSubmissionsTable.jsx";
 import SocialLink from "../../components/SocialLink/SocialLink.jsx";
+import UserLogic from "./User.js";
 
 function User({ imageReducer }) {
   /* ===== CONTEXTS ===== */
@@ -18,105 +21,81 @@ function User({ imageReducer }) {
   /* ===== VARIABLES ===== */
   const SOCIAL_SIZE = 50;
 
-  /* ===== MEMOS ===== */
-  const searchParams = useMemo(() => {
-    const params = new URLSearchParams();
-    params.append("profile_id", profile.id);
-    return params;
-  }, [profile.id]);
+  /* ===== MEMOS & FUNCTIONS ===== */
+  
+  // memos & functions from the js file
+  const { searchParams, socials, details, onGameRowClick } = UserLogic(); 
+
+  // helper functions
+  const { getUrlType } = EmbedHelper();
 
   /* ===== USER COMPONENT ===== */
   return (
-    <div className="user">
-      <div className="user-info">
+    <div className={ styles.user }>
 
-        <h1>User Information</h1>
+      { /* User information: render general information about the user */ }
+      <Container title="User Information" largeTitle>
 
         { /* Contact - Render the user's social media information. */ }
-        <div className="user-info-contact">
-          <h2>Socials</h2>
+        <h2>Socials</h2>
+        <Items items={ socials } emptyMessage="This user has no socials.">
+          <div className={ styles.socials }>
+            <SocialLink name="youtube" username={ profile.youtube_handle } size={ SOCIAL_SIZE } />
+            <SocialLink name="twitch" username={ profile.twitch_username } size={ SOCIAL_SIZE } />
+            <SocialLink name="twitter" username={ profile.twitter_handle } size={ SOCIAL_SIZE } />
+            <DiscordLogo discord={ profile.discord } size={ SOCIAL_SIZE } />
+          </div>
+        </Items>
 
-          { /* Render socials if any exist. */ }
-          { profile.youtube_handle || profile.twitch_username || profile.twitter_handle || profile.discord ?
-            <div className="user-info-socials">
-              <SocialLink name="youtube" username={ profile.youtube_handle } size={ SOCIAL_SIZE } />
-              <SocialLink name="twitch" username={ profile.twitch_username } size={ SOCIAL_SIZE } />
-              <SocialLink name="twitter" username={ profile.twitter_handle } size={ SOCIAL_SIZE } />
-              <DiscordLogo discord={ profile.discord } size={ SOCIAL_SIZE } />
-            </div>
-          :
-
-          // Otherwise, render a message telling the client that the user has no socials.
-            <div className="user-missing">
-              <p><i>This user has no socials.</i></p>
-            </div>
-          }
-
-        </div>
+        <hr />
 
         { /* Details - Render the user's details. */ }
         <h2>Details</h2>
+        <Items items={ details } emptyMessage="This user has no details.">
+          { profile.bio &&
+            <p><b>About Me:</b> { profile.bio }</p>
+          }
+          { profile.birthday &&
+            <p><b>Birthday:</b> { profile.birthday }</p>
+          }
+        </Items>
 
-        { /* Render details if any exist. */ }
-        { profile.bio || profile.birthday ?
-          <div className="user-about-me">
-            { /* A user is not required to have a bio. Only render bio if it exists. */ }
-            { profile.bio &&
-              <p><b>About Me:</b> { profile.bio }</p>
-            }
-  
-            { /* A user is not required to have a birthday. Only render birthday if it exists. */ }
-            { profile.birthday &&
-              <p><b>Birthday:</b> { profile.birthday }</p>
-            }
+      </Container>
 
-          </div>
-        :
-          // Otherwise, render a message telling the client that the user has no details.
-          <div className="user-missing">
-            <p><i>This user has no details.</i></p>
-          </div>
-        }
-
-      </div>
-      
-      { /* If a user has a featured video, render it */ }
+      {/* Featured video - Render the user's featured video, if there is one */}
       { profile.featured_video &&
-        <div className="user-info">
-          <h1>Featured Video</h1>
-          <div className="user-featured-video">
+        <Container title="Featured Video" largeTitle>
+          <div className={ getUrlType(profile.featured_video) !== "twitter" ? styles.video : styles.twitter }>
             <EmbededVideo url={ profile.featured_video } />
           </div>
           { profile.video_description &&
             <p>{ profile.video_description }</p>
           }
-        </div>
+        </Container>
       }
 
-      { /* Render the user's recent submissions */ }
-      <div className="user-info">
-        <h1><Link to={ `/recent-submissions?profile_id=${ profile.id }` }>Recent Submissions</Link></h1>
+      { /* Recent submissions - Render the user's recent submissions */ }
+      <Container title="Recent Submissions" href={ `/recent-submissions?profile_id=${ profile.id }` } largeTitle>
         <RecentSubmissionsTable searchParams={ searchParams } />
-      </div>
+      </Container>
 
-      { /* If a user moderates at least 1 game, render a link to the game page for each game */ }
+      { /* Games moderated - Render each game the user moderators, if at least one exists */ }
       { profile.moderated_games.length > 0 &&
-        <div className="user-info">
-          <h1>Games Moderated</h1>
-          <div className="user-games">
-            { profile.moderated_games.map(game => {
-              return (
-                <div className="user-game">
-                  <Link to={ `/games/${ game.abb }` }>
-                    { <BoxArt game={ game } imageReducer={ imageReducer } width={ 80 } /> }
-                    <span>{ game.name }</span>
-                  </Link>
-                </div>
-              );
-            })}
-          </div>
-        </div>
+        <Container title="Games Moderated" largeTitle>
+          { profile.moderated_games.map((game, index) => {
+            return (
+              <GameRow 
+                game={ game } 
+                imageReducer={ imageReducer } 
+                index={ index }
+                onClick={ onGameRowClick } 
+                key={ game.abb } 
+              />
+            );
+          })}
+        </Container>
       }
+
     </div>
   );
 };
