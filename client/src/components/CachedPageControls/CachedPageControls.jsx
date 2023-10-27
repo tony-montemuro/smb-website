33271d0a32776febdba9fdee48d85cf1f2913648
Tony Montemuro <tonyamontemuro@gmail.com>
2023-10-27 00:17:33 -0500

    added styling to the cached page control component

--- client/src/components/CachedPageControls/CachedPageControls.jsx
+++ client/src/components/CachedPageControls/CachedPageControls.jsx
@@ -1,5 +1,5 @@
 /* ===== IMPORTS ===== */
-import "./CachedPageControls.css";
+import styles from "./CachedPageControls.module.css";
 import CachedPageControlsLogic from "./CachedPageControls.js";
 
 function CachedPageControls({ items, itemsPerPage, pageNum, setPageNum, itemsName }) {
@@ -10,19 +10,17 @@ function CachedPageControls({ items, itemsPerPage, pageNum, setPageNum, itemsNam
 
   /* ===== CACHED PAGE CONTROLS COMPONENT ===== */ 
   return items.length > itemsPerPage &&
-    <div className="cached-page-controls">
+    <div className={ styles.cachedPageControls }>
 
       { /* Cached page viewer - render the set of pages shown on the current page */ }
-      <div className="cached-page-viewer">
+      <span>
         Showing { getStartAndEnd(itemsPerPage, items.length, pageNum).start } to&nbsp;
         { getStartAndEnd(itemsPerPage, items.length, pageNum).end } of { items.length } { itemsName }
-      </div>
+      </span>
 
       { /* Cached page controller - render buttons to navigate to the previous and next page, as well as a dropdown 
       for the user to select any valid page */ }
-      <div className="cached-page-controller">
-
-        { /* Previous page button */ }
+      <div className={ styles.controller }>
         <button 
           type="button"
           onClick={ () => setPageNum(pageNum-1) } 
@@ -30,15 +28,11 @@ function CachedPageControls({ items, itemsPerPage, pageNum, setPageNum, itemsNam
         >
           Previous Page
         </button>
-
-        { /* Page selector */ }
         <select value={ pageNum } onChange={ (e) => setPageNum(parseInt(e.target.value)) }>
           { [...Array(getMaxPage(items.length, itemsPerPage)).keys()].map(num => {
             return <option value={ num+1 } key={ num+1 }>{ num+1 }</option>;
           })}
         </select>
-
-        { /* Next page button */ }
         <button
           type="button"
           onClick={ () => setPageNum(pageNum+1) } 
@@ -46,7 +40,6 @@ function CachedPageControls({ items, itemsPerPage, pageNum, setPageNum, itemsNam
         >
           Next Page
         </button>
-
       </div>
 
     </div>
