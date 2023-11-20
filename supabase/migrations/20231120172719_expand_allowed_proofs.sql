d7dccd9a9f9e4620b33f5c8cf1b887b8a40b6305
Tony Montemuro <tonyamontemuro@gmail.com>
2023-11-20 11:41:36 -0600

    expanded the backend proof validation function to allow for imgur links

---
+++ supabase/migrations/20231120172719_expand_allowed_proofs.sql
@@ -0,0 +1,12 @@
+CREATE OR REPLACE FUNCTION validate_video_url(url text)
+RETURNS boolean
+LANGUAGE sql
+AS $$
+  SELECT CASE 
+    WHEN url ~ '^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$' THEN TRUE
+    WHEN url ~ '^(?:https?:\/\/)?(?:www\.)?(?:twitch\.tv\/(?:videos\/(\d{7,11})(?:\?t=([\dhms]+))?[^\s&]*(?:\?[\w=&-]*)?|[a-zA-Z0-9][\w]{2,24}\/clip\/([a-zA-Z0-9_-]+)(?:\?[\w=&-]*)?)|clips\.twitch\.tv\/([a-zA-Z0-9_-]+))(?:\?[\w=&-]*)?$' THEN TRUE
+    WHEN url ~ '^(?:https?:\/\/)?(?:www\.)?(?:twitter\.com|x\.com)\/(?:#!\/)?(\w+)\/status(?:es)?\/(\d+)[/\S]*$' THEN TRUE
+    WHEN url ~ '^(?:https?:\/\/)?(?:www\.)?imgur\.com/$' THEN TRUE
+    ELSE FALSE
+  END
+$$;
