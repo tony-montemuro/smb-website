3e630c61b242400879dd8ba3a4a31a4113ae2fc0
Tony Montemuro <tonyamontemuro@gmail.com>
2023-11-21 16:39:53 -0600

    removed unused values from 'category_t' enum

--- supabase/migrations/20231120033543_additional_bm_categories.sql
+++
@@ -1,5 +0,0 @@
-ALTER TYPE category_t
-ADD VALUE 'perfect_special'; 
-
-ALTER TYPE category_t
-ADD VALUE 'perfect_special_jump';
---
+++ supabase/migrations/20231121200715_add_bm_category_types.sql
@@ -0,0 +1,11 @@
+ALTER TYPE category_t
+ADD VALUE 'main_jump' BEFORE 'party';
+
+ALTER TYPE category_t
+ADD VALUE 'special' BEFORE 'party'; 
+
+ALTER TYPE category_t
+ADD VALUE 'special_jump' BEFORE 'party'; 
+
+ALTER TYPE category_t
+ADD VALUE 'ranking' BEFORE 'party';
