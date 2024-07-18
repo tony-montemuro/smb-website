fe05c8cbf0de7e60b4b36a2ccf687a89b55e4cbd
Tony Montemuro <tonyamontemuro@gmail.com>
2024-07-18 06:56:05 -0500

    WIP: progress on level input

--- client/src/pages/StructureForm/LevelList/LevelInput.jsx
+++ client/src/pages/StructureForm/LevelList/LevelInput.jsx
@@ -1,14 +1,22 @@
 /* ===== IMPORTS ===== */
 import styles from "./LevelList.module.css";
+import Checkbox from "@mui/material/Checkbox";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormGroup from "@mui/material/FormGroup";
+import LevelInputLogic from "./LevelInput.js";
 import FrontendHelper from "../../../helper/FrontendHelper";
 import TextField from "@mui/material/TextField";
 
 function LevelInput({ id, level, mode, category, formData, handleChange }) {
   /* ===== FUNCTIONS ===== */
+  const scoreChartTypes = ["both", "score"];
+  const timeChartTypes = ["both", "time"];
+
+  // functions from the js file
+  const { timerTypeB2F } = LevelInputLogic();
 
   // helper functions
   const { capitalize } = FrontendHelper();
-  console.log(formData);
 
   /* ===== LEVEL INPUT COMPONENT ===== */
   return (
@@ -40,6 +48,56 @@ function LevelInput({ id, level, mode, category, formData, handleChange }) {
           </option>
         ))}
       </TextField>
+
+      <TextField
+        id={ `${ id }_timer_type` }
+        label="Timer Type"
+        onChange={ e => handleChange() }
+        select
+        SelectProps={ { native: true } }
+        sx={ { width: "fit-content" } }
+        value={ level.timer_type }
+        variant="filled"
+      >
+        { formData.timerTypes.map(timerType => (
+          <option
+            key={ timerType }
+            value={ timerType }
+          >
+            { timerTypeB2F(timerType) }
+          </option>
+        ))}
+      </TextField>
+
+      <FormGroup>
+        <FormControlLabel 
+          control={ 
+            <Checkbox 
+              checked={ scoreChartTypes.includes(level.ascending) } 
+              disabled={ level.chartType === "time" }
+              id={ `${ id }_ascending_score` } 
+              onChange={ handleChange } 
+              inputProps={{ "aria-label": "controlled" }} 
+            />
+          } 
+          label="Ascend Score" 
+        />
+      </FormGroup>
+
+      <FormGroup>
+        <FormControlLabel 
+          control={ 
+            <Checkbox 
+              checked={ timeChartTypes.includes(level.ascending) } 
+              disabled={ level.chartType === "score" }
+              id={ `${ id }_ascending_time` } 
+              onChange={ handleChange } 
+              inputProps={{ "aria-label": "controlled" }} 
+            />
+          } 
+          label="Ascend Time"
+        />
+      </FormGroup>
     </div>
   );
 };
