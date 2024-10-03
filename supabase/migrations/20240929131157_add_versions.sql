9585c839de000cceb6a05b5a6f16e68761d51c6c
Tony Montemuro <tonyamontemuro@gmail.com>
2024-10-03 07:00:43 -0500

    WIP: working on backend update to add versions to levelboard

--- supabase/migrations/20240929131157_add_versions.sql
+++ supabase/migrations/20240929131157_add_versions.sql
@@ -184,6 +184,7 @@ ALTER TRIGGER submission_before_insert_trigger ON submission RENAME TO submissio
 -- Now, we need to update RPCs to allow for version specification
 
 -- First, `get_ranked_submissions`. Many relevant RPCs depend on this.
+DROP FUNCTION get_ranked_submissions(text, text, boolean, boolean);
 CREATE OR REPLACE FUNCTION get_ranked_submissions(game_name text, category_name text, is_score boolean, live_only boolean, version_key int)
 RETURNS TABLE (
   game_id text,
@@ -253,6 +254,7 @@ AS $$
 $$;
 
 -- Next, medals RPC.
+DROP FUNCTION get_medals(text, text, boolean);
 CREATE OR REPLACE FUNCTION get_medals(abb text, category text, score boolean, version int)
 RETURNS json
 LANGUAGE sql
@@ -293,6 +295,7 @@ FROM (
 $$;
 
 -- Next, totals RPC.
+DROP FUNCTION get_totals(text, text, boolean, boolean);
 CREATE OR REPLACE FUNCTION get_totals(abb text, category text, score boolean, live_only boolean, version int)
 RETURNS json
 LANGUAGE plv8
@@ -371,6 +374,7 @@ AS $$
 $$;
 
 -- Next, get_records_submissions RPC, a dependency of records RPC.
+DROP FUNCTION get_record_submissions(text, text, boolean, boolean);
 CREATE OR REPLACE FUNCTION get_record_submissions(game_name text, category_name text, is_score boolean, live_only boolean, version_key int)
 RETURNS TABLE (
   id timestamptz,
@@ -429,6 +433,7 @@ AS $$
 $$;
 
 -- Next, records RPC.
+DROP FUNCTION get_records(text, text, boolean, boolean);
 CREATE OR REPLACE FUNCTION get_records(abb text, category text, score boolean, live_only boolean, version int)
 RETURNS json
 LANGUAGE plv8
@@ -489,4 +494,73 @@ AS $$
 
   // -- finally, return record table
   return recordTable;
+$$;
+
+-- Next, level board RPC.
+DROP FUNCTION get_chart_submissions(text, text, text, boolean);
+CREATE OR REPLACE FUNCTION get_chart_submissions(game text, category_name text, level text, is_score boolean, version_key int) 
+RETURNS json
+LANGUAGE sql
+AS $$
+  WITH chart_submissions AS (
+    SELECT *
+    FROM submission s
+    WHERE s.game_id = game AND s.category = category_name AND s.level_id = level AND s.score = is_score AND (CASE WHEN version IS NOT NULL THEN version = version_key ELSE true END)
+  ),
+  ranked_by_profile AS (
+    SELECT cs.id, cs.record, cs.live, ROW_NUMBER() OVER (PARTITION BY cs.profile_id ORDER BY cs.submitted_at DESC, cs.id DESC) AS rn
+    FROM chart_submissions cs
+    WHERE cs.tas = false
+  ),
+  ranked AS (
+    SELECT rbp.id
+    FROM ranked_by_profile rbp
+    WHERE rbp.rn = 1
+  ),
+  live_ranked AS (
+    SELECT rbp.id, RANK() OVER (ORDER BY rbp.record DESC) AS "position"
+    FROM ranked_by_profile rbp
+    WHERE rbp.rn = 1 AND rbp.live = true
+  ),
+  ranked_by_profile_tas AS (
+    SELECT cs.id, cs.record, cs.live, ROW_NUMBER() OVER (PARTITION BY cs.profile_id ORDER BY cs.submitted_at DESC) AS rn
+    FROM chart_submissions cs
+    WHERE cs.tas = true
+  ),
+  tas_ranked AS (
+    SELECT rbpt.id
+    FROM ranked_by_profile_tas rbpt
+    WHERE rbpt.rn = 1
+  )
+  SELECT COALESCE((json_agg(row_to_json(chart_row))), '[]'::json)
+  FROM (
+    SELECT
+      (SELECT jsonb_build_object('creator', jsonb_build_object('country', p.country, 'id', p.id, 'username', p.username)) FROM approve a INNER JOIN profile p ON a.creator_id = p.id WHERE a.submission_id = cs.id) AS "approve",
+      cs.comment,
+      cs.id,
+      cs.live,
+      CASE
+        WHEN live_ranked.position = 3 THEN 'bronze'
+        WHEN live_ranked.position = 2 THEN 'silver'
+        WHEN live_ranked.position = 1 AND EXISTS (SELECT 1 FROM live_ranked lr WHERE lr.position = 1 AND lr.id <> live_ranked.id) THEN 'gold'
+        WHEN live_ranked.position = 1 THEN 'platinum'
+        ELSE NULL
+      END as medal,
+      cs.mod_note,
+      (SELECT row_to_json(monkey_row) FROM (SELECT m.id, m.monkey_name FROM monkey m WHERE cs.monkey_id = m.id) AS monkey_row) monkey,
+      CASE WHEN (ranked.id IS NULL AND tas_ranked.id IS NULL) THEN true ELSE false END AS obsolete,
+      (SELECT row_to_json(platform_row) FROM (SELECT pl.id, pl.platform_abb, pl.platform_name FROM platform pl WHERE cs.platform_id = pl.id) AS platform_row) platform,
+      (SELECT row_to_json(profile_row) FROM (SELECT pr.id, pr.username, pr.country FROM profile pr WHERE cs.profile_id = pr.id) AS profile_row) profile,
+      cs.proof,
+      cs.record,
+      (SELECT row_to_json(region_row) FROM (SELECT rg.id, rg.region_name FROM region rg WHERE cs.region_id = rg.id) AS region_row) region,
+      (SELECT row_to_json(report_row) FROM (SELECT rp.creator_id FROM report rp WHERE rp.submission_id = cs.id) AS report_row) report,
+      cs.submitted_at,
+      cs.tas
+    FROM chart_submissions cs
+    LEFT OUTER JOIN ranked ON cs.id = ranked.id
+    LEFT OUTER JOIN live_ranked ON cs.id = live_ranked.id
+    LEFT OUTER JOIN tas_ranked ON cs.id = tas_ranked.id
+    ORDER BY cs.record DESC, cs.submitted_at ASC
+  ) chart_row
 $$;
