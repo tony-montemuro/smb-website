68f0020a86ce2bd73ce458ce9b0fcaa158676c9e
tony.montemuro <tonyamontemuro@gmail.com>
2025-04-07 21:18:49 -0500

    FIX: get reports working for normal users

--- client/src/components/SubmissionDetails/ReportForm/ReportForm.js
+++ client/src/components/SubmissionDetails/ReportForm/ReportForm.js
@@ -40,24 +40,24 @@ const ReportForm = () => {
         // first, update the form to prevent multiple submissions
         e.preventDefault();
         setSubmitting(true);
-    
+
         // define our report object
         const report = {
             submission_id: submission.id,
             creator_id: user.profile.id,
             message: message
         };
-          
+
         // now, let's add the report to the database
         try {
             await insertReport(report);
-        
+
             // now, make concurrent calls to both update user, as well as the boards of the caller component
             await Promise.all([updateUser(user.id), updateBoards()])
             await updateUser(user.id);
             addMessage("The submission was successfully reported! Please give the moderation team a few days to look it over.", "success", 10000);
             closePopup();
-    
+
         } catch (error) {
             // otherwise, render an error message, and set the submitting state back to false
             addMessage("There was a problem reporting this submission.", "error", 8000);
