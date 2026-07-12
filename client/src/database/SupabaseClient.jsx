cd38e60b1071a8fbb79b20c73bbe7ec7dd829538
tony.montemuro <tonyamontemuro@gmail.com>
2026-07-12 18:49:51 -0500

    migrate from process.env to import.meta.env for loading env vars

--- client/src/database/SupabaseClient.jsx
+++ client/src/database/SupabaseClient.jsx
@@ -2,8 +2,8 @@
 import { createClient } from "@supabase/supabase-js";
 
 /* ===== VARIABLES ===== */
-const SUPABASE_URL = process.env.VITE_APP_SUPABASE_URL;
-const SUPABASE_ANON_KEY = process.env.VITE_APP_SUPABASE_ANON_KEY;
+const SUPABASE_URL = import.meta.env.VITE_APP_SUPABASE_URL;
+const SUPABASE_ANON_KEY = import.meta.env.VITE_APP_SUPABASE_ANON_KEY;
 
 /* ===== EXPORTS ===== */
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
