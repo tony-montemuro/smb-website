e088978c9f3b99bbd986d45bfab1776ed7bbc121
Tony Montemuro <75276447+tony-montemuro@users.noreply.github.com>
2026-07-15 07:28:03 -0500

    MOD: update low risk dependencies (#43)
    ## What changed?
    
    Update / remove unused dependencies from SPA.
    
    ## Why the change?
    
    Part of an effort to modernize SMBElite, decreasing the risk of security threats.
    
    Work Done:
    
    * remove unused web-vitals package
    
    * update flag-icons 6.6.4 -> 6.15.0
    
    * update dayjs 1.11.10 -> 1.11.21

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -6,7 +6,6 @@ import { MuiTheme } from "./utils/Themes";
 import { ThemeProvider } from "@mui/material";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import reportWebVitals from "./reportWebVitals";
 
 // create root, and render
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -19,8 +18,3 @@ root.render(
     </ThemeProvider>
   </BrowserRouter>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
