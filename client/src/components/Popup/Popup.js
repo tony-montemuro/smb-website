100c772e6756ce0acf8b8611db451b56914ef40b
tony.montemuro <tonyamontemuro@gmail.com>
2026-07-22 06:40:39 -0500

    update popup closing behavior to be compatible with v8 date pickers

--- client/src/components/Popup/Popup.js
+++ client/src/components/Popup/Popup.js
@@ -1,50 +1,51 @@
 const Popup = (setRenderPopup, innerRef) => {
-    /* ===== FUNCTIONS ===== */
+  /* ===== FUNCTIONS ===== */
 
-    // FUNCTION 1: closePopup - simple function that, when called, will close a particular popup component
-    // PRECONDITIONS (1 condition):
-    // the `renderPopup` state should be set to true when this function is called
-    // POSTCONDITIONS (1 possible outcome):
-    // the `renderPopup` state is set to false by calling the setter functions with the `false` argument
-    const closePopup = () => setRenderPopup(false);
+  // FUNCTION 1: closePopup - simple function that, when called, will close a particular popup component
+  // PRECONDITIONS (1 condition):
+  // the `renderPopup` state should be set to true when this function is called
+  // POSTCONDITIONS (1 possible outcome):
+  // the `renderPopup` state is set to false by calling the setter functions with the `false` argument
+  const closePopup = () => setRenderPopup(false);
 
-    // FUNCTION 2: handleClick - code that executes each time the user performs the "mousedown" action when the popup is open
-    // PRECONDITIONS (1 parameter):
-    // 1.) e: an event object generated each time the user performs the "mousedown" action while the popup is open
-    // POSTCONDITIONS (2 possible outcomes):
-    // if we are clicking within the popup, this function does nothing
-    // if we are clicking outside the popup, this function closes the popup
-    const handleClick = e => {
-        if (!innerRef.current.contains(e.target)) {
-            // handle exception for date picker calendar, which renders outside popup
-            const desktopDatePicker = document.querySelector(".MuiPickersPopper-root");
-            const mobileDatePicker = document.querySelector(".MuiModal-root");
-            const clickWithinDesktopDatePicker = desktopDatePicker && desktopDatePicker.contains(e.target);
-            const clickWithinMobileDatePicker = mobileDatePicker && mobileDatePicker.contains(e.target);
-            if (!(clickWithinDesktopDatePicker || clickWithinMobileDatePicker)) {
-                closePopup();
-            }
-        }
+  // FUNCTION 2: handleClick - code that executes each time the user performs the "mousedown" action when the popup is open
+  // PRECONDITIONS (1 parameter):
+  // 1.) e: an event object generated each time the user performs the "mousedown" action while the popup is open
+  // POSTCONDITIONS (2 possible outcomes):
+  // if we are clicking within the popup, this function does nothing
+  // if we are clicking outside the popup, this function closes the popup
+  const handleClick = e => {
+    console.log("tflol")
+    if (!innerRef.current.contains(e.target)) {
+      // handle exception for date picker calendar, which renders outside popup
+      const desktopDatePicker = document.querySelector(".MuiPickerPopper-root");
+      const mobileDatePicker = document.querySelector(".MuiModal-root");
+      const clickWithinDesktopDatePicker = desktopDatePicker && desktopDatePicker.contains(e.target);
+      const clickWithinMobileDatePicker = mobileDatePicker && mobileDatePicker.contains(e.target);
+      if (!(clickWithinDesktopDatePicker || clickWithinMobileDatePicker)) {
+        closePopup();
+      }
     }
+  }
 
-    // FUNCTION 3: handleTouch - code that executes each time a mobile user performs the "touchend" action when the popup is open
-    // PRECONDITIONS (1 parameter):
-    // 1.) e: an event object generated each time the user performs the "mousedown" action while the popup is open
-    // POSTCONDITIONS (2 possible outcomes):
-    // if we are touching within the popup, this function does nothing
-    // if we are touching outside the popup, this function closes the popup
-    const handleTouch = e => {
-        if (!innerRef.current.contains(e.changedTouches[0].target)) {
-            // handle exception for date picker calendar, which renders outside popup
-            const datepicker = document.querySelector(".MuiModal-root");
-            const clickWithinDatePicker = datepicker && datepicker.contains(e.changedTouches[0].target);
-            if (!clickWithinDatePicker) {
-                closePopup();
-            }
-        }
-    };
+  // FUNCTION 3: handleTouch - code that executes each time a mobile user performs the "touchend" action when the popup is open
+  // PRECONDITIONS (1 parameter):
+  // 1.) e: an event object generated each time the user performs the "mousedown" action while the popup is open
+  // POSTCONDITIONS (2 possible outcomes):
+  // if we are touching within the popup, this function does nothing
+  // if we are touching outside the popup, this function closes the popup
+  const handleTouch = e => {
+    if (!innerRef.current.contains(e.changedTouches[0].target)) {
+      // handle exception for date picker calendar, which renders outside popup
+      const datepicker = document.querySelector(".MuiModal-root");
+      const clickWithinDatePicker = datepicker && datepicker.contains(e.changedTouches[0].target);
+      if (!clickWithinDatePicker) {
+        closePopup();
+      }
+    }
+  };
 
-    return { closePopup, handleClick, handleTouch };
+  return { closePopup, handleClick, handleTouch };
 };
 
 /* ===== EXPORTS ===== */
