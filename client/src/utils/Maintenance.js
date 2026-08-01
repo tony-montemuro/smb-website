ced0cff6bf9bb5e123d12b84aed34de2e46f0358
tony.montemuro <tonyamontemuro@gmail.com>
2026-08-01 17:19:16 -0500

    MOD: announce postgresql 17 maintenance window

--- client/src/utils/Maintenance.js
+++ client/src/utils/Maintenance.js
@@ -1,3 +1,4 @@
 /* ===== EXPORTS ===== */
 // Set to a message to announce planned downtime site-wide; `null` hides the banner.
-export const MAINTENANCE_MESSAGE = null;
+export const MAINTENANCE_MESSAGE =
+  "Scheduled maintenance: SMBElite will be unavailable from 12:00 AM to 1:30 AM ET on August 2nd, 2026. Leaderboards and submissions will not work during this window.";
