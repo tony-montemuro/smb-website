1e90579291acf2be7986d18f0bbd01c9878d150a
tony.montemuro <tonyamontemuro@gmail.com>
2026-08-01 17:42:42 -0500

    MOD: clear maintenance message

--- client/src/utils/Maintenance.js
+++ client/src/utils/Maintenance.js
@@ -1,4 +1,3 @@
 /* ===== EXPORTS ===== */
 // Set to a message to announce planned downtime site-wide; `null` hides the banner.
-export const MAINTENANCE_MESSAGE =
-  "Scheduled maintenance: SMBElite will be unavailable from 12:00 AM to 1:30 AM ET on August 2nd, 2026. Leaderboards and submissions will not work during this window.";
+export const MAINTENANCE_MESSAGE = null;
